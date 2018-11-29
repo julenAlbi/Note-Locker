e2a252909628199facfbff87c5f636308fd2de65
Julen <julenalbizuri3@gmail.com>
2018-11-29 22:36:27 +0100

    ActivityExtension file created with some extensions and activity launcher created.

---
+++ app/src/main/java/com/example/julen/nativeappsproject/LauncherActivity.kt
@@ -0,0 +1,21 @@
+package com.example.julen.nativeappsproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.julen.nativeappsproject.extensions.startHomeActivity
+import com.example.julen.nativeappsproject.extensions.startSignUpActivity
+import com.example.julen.nativeappsproject.storage.SharedPreferencesManager
+
+class LauncherActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+
+        if (SharedPreferencesManager(this).isPasswordSaved())
+            startHomeActivity()
+        else
+            startSignUpActivity()
+    }
+}
---
+++ app/src/main/java/com/example/julen/nativeappsproject/extensions/ActivityExtensions.kt
@@ -0,0 +1,2 @@
+package com.example.julen.nativeappsproject.extensions
+
