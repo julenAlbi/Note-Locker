6b007c6c8a2d06dc86a0f25850c495a241c8a42d
Julen <julenalbizuri3@gmail.com>
2018-12-01 18:27:50 +0100

    ChipherManager => InitializationVector added to encrypt and decrypt methods.

--- app/src/main/java/com/example/julen/nativeappsproject/encription/CipherManager.kt
+++ app/src/main/java/com/example/julen/nativeappsproject/encription/CipherManager.kt
@@ -1,8 +1,10 @@
 package com.example.julen.nativeappsproject.encription
 
 import android.util.Base64
+import android.util.Log
 import java.security.Key
 import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
 
 class CipherManager(val transformation: String) {
 
@@ -10,19 +12,45 @@ class CipherManager(val transformation: String) {
         var TRANSFORMATION_ASYMMETRIC = "RSA/ECB/PKCS1Padding"
 
         var TRANSFORMATION_SYMMETRIC = "AES/CBC/PKCS7Padding"
+
+        val IV_SEPARATOR = "]"
     }
 
     val cipher: Cipher = Cipher.getInstance(transformation)
 
-    fun encrypt(data: String, key: Key?): String {
+    fun encrypt(data: String, key: Key?, useInitializationVector: Boolean = false): String {
         cipher.init(Cipher.ENCRYPT_MODE, key)
+
+        var result = ""
+        if (useInitializationVector) {
+            val iv = cipher.iv
+            val ivString = Base64.encodeToString(iv, Base64.DEFAULT)
+            result = ivString + IV_SEPARATOR
+        }
+
         val bytes = cipher.doFinal(data.toByteArray())
-        return Base64.encodeToString(bytes, Base64.DEFAULT)
+        result += Base64.encodeToString(bytes, Base64.DEFAULT)
+
+        return result
     }
 
-    fun decrypt(data: String, key: Key?): String {
-        cipher.init(Cipher.DECRYPT_MODE, key)
-        val encryptedData = Base64.decode(data, Base64.DEFAULT)
+    fun decrypt(data: String, key: Key?, useInitializationVector: Boolean = false): String {
+        var encodedString: String
+
+        if (useInitializationVector) {
+            val split = data.split(IV_SEPARATOR.toRegex())
+            if (split.size != 2) throw IllegalArgumentException("Passed data is incorrect. There was no IV specified with it.")
+
+            val ivString = split[0]
+            encodedString = split[1]
+            val ivSpec = IvParameterSpec(Base64.decode(ivString, Base64.DEFAULT))
+            cipher.init(Cipher.DECRYPT_MODE, key, ivSpec)
+        } else {
+            encodedString = data
+            cipher.init(Cipher.DECRYPT_MODE, key)
+        }
+
+        val encryptedData = Base64.decode(encodedString, Base64.DEFAULT)
         val decodedData = cipher.doFinal(encryptedData)
         return String(decodedData)
     }
