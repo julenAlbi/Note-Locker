c0dd71061afc71087e5dcf4ddeb862fa6da2f527
Julen <julenalbizuri3@gmail.com>
2018-12-02 15:24:57 +0100

    addNoteFragment created.

--- app/src/main/java/com/example/julen/nativeappsproject/addNoteFragment.kt
+++ app/src/main/java/com/example/julen/nativeappsproject/addNoteFragment.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.julen.nativeappsproject.model.Note
+import kotlinx.android.synthetic.main.fragment_add_note.view.*
 import kotlinx.android.synthetic.main.fragment_note.view.*
 
-class NoteFragment : Fragment() {
+class addNoteFragment : Fragment() {
 
     /**
      * The note this fragment is representing
@@ -22,10 +22,10 @@ class NoteFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         arguments!!.let {
-            if (it.containsKey(ARG_NOTE)) {
+            if (it.containsKey(NoteFragment.ARG_NOTE)) {
                 // Load the note specified by the fragment
                 // arguments.
-                note = it.getSerializable(ARG_NOTE) as Note
+                note = it.getSerializable(NoteFragment.ARG_NOTE) as Note
             }
         }
 
@@ -37,38 +37,37 @@ class NoteFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_note, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_add_note, container, false)
 
         //activity?.toolbar_note?.title = note.alias
 
         // Show the note content
         // The let construct calls the specified function block with this value as its argument
         // and returns its result.
-        note.let {
-            rootView.notealias.text = it.alias
-            rootView.createdDate.text = it.createDate.toString("dd/mm/yyyy")
-            rootView.updatedDate.text = it.updateDate.toString("dd/mm/yyyy")
-            rootView.notetext.append(it.secret.toString())
+        note?.let {
+            rootView.noteNameLabel.text = it.alias
+            rootView.lockedCheckBox.isChecked = it.locked
+            rootView.noteTextEditing.append(it.secret.toString())
         }
         return rootView
     }
 
-    companion object {
 
+    companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param note the note that is going to display.
+         * @param note the note that is going to be edited. If note is null, a new note is going to be created.
          * @return A new instance of fragment addNoteFragment.
-         */
-        const val ARG_NOTE = "item_id"
-
+        */
         @JvmStatic
-        fun newInstance(note : Note) = NoteFragment().apply {
-                    arguments = Bundle().apply {
-                        putSerializable(ARG_NOTE, note)
-                    }
+        fun newInstance(note : Note? = null) = NoteFragment().apply {
+            note?.let {
+                arguments = Bundle().apply {
+                    putSerializable(NoteFragment.ARG_NOTE, note)
                 }
+            }
+        }
     }
 }
