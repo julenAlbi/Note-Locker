c235794444e904e090bfd3b9914156a590860284
iblAneluJ <julenalbizuri3@gmail.com>
2018-12-24 13:51:02 +0100

    Repository property and database methods added to NoteViewModel. If 'noteSelected' parameter is null, NoteViewModel currently contains a note.

--- app/src/main/java/com/example/julen/nativeappsproject/note/NoteViewModel.kt
+++ app/src/main/java/com/example/julen/nativeappsproject/note/NoteViewModel.kt
@@ -5,21 +5,36 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import android.util.Log
-import com.example.julen.nativeappsproject.NoteDataBase
 import com.example.julen.nativeappsproject.model.Note
+import com.example.julen.nativeappsproject.storage.NoteRepository
 
-class NoteViewModel(noteSelected : Note, application: Application) : AndroidViewModel(application) {
+class NoteViewModel(noteSelected : Note?, application: Application) : AndroidViewModel(application) {
 
     var note = MutableLiveData <Note>()
+    private lateinit var noteRepo: NoteRepository
 
     init {
-        this.note.value = noteSelected
-        Log.d("NOTE VALUEEE:", note.value.toString())
+
+        if(noteSelected != null){
+            this.note.value = noteSelected
+        }
+        noteRepo = NoteRepository(application)
+    }
+
+    fun update(){
+        noteRepo.updateNote(note.value!!)
+    }
+
+    fun insert(){
+        noteRepo.insertNote(note.value!!)
+    }
+
+    fun delete(){
+        noteRepo.deleteNote(note.value!!)
     }
 }
 
-class NoteViewModelFactory(private val note : Note, private val application: Application) : ViewModelProvider.NewInstanceFactory() {
+class NoteViewModelFactory(private val note : Note?, private val application: Application) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NoteViewModel(note, application) as T
     }
