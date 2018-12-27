78d2fb6ef5afac9d7d4f95cac35d0af00c5b6aa8
iblAneluJ <julenalbizuri3@gmail.com>
2018-12-27 15:11:55 +0100

    Stack cleared on creating signupActivity and noteListActivity.

--- app/src/main/java/com/example/julen/nativeappsproject/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/example/julen/nativeappsproject/extensions/ActivityExtensions.kt
@@ -2,6 +2,8 @@ package com.example.julen.nativeappsproject.extensions
 
 import android.app.Activity
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.widget.Toast
 import com.example.julen.nativeappsproject.note.NoteActivity
 import com.example.julen.nativeappsproject.note.NoteFragment
@@ -13,16 +15,16 @@ fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT){
     Toast.makeText(this, message, duration).show()
 }
 
-fun Activity.startHomeActivity() = startActivity(NoteListActivity::class.java)
+fun Activity.startHomeActivity() = startActivity(NoteListActivity::class.java, clearTask = true)
 
-fun Activity.startSignUpActivity() = startActivity(SignUpActivity::class.java)
+fun Activity.startSignUpActivity() = startActivity(SignUpActivity::class.java, clearTask = true)
 
 fun Activity.startNoteActivity(note : Note? = null, mode : String){
     var notee = if(mode.equals(NoteActivity.ADD_NOTE)) Note(0,"","") else note
     startActivity(NoteActivity::class.java, notee, mode)
 }
 
-private fun Activity.startActivity(cls: Class<*>, note : Note? = null, mode: String? = null) {
+private fun Activity.startActivity(cls: Class<*>, note : Note? = null, mode: String? = null, clearTask: Boolean = false) {
     val intent = Intent(this, cls).apply {
         note?.let {
             putExtra(NoteFragment.ARG_NOTE, it)
@@ -31,5 +33,6 @@ private fun Activity.startActivity(cls: Class<*>, note : Note? = null, mode: Str
             putExtra(NoteActivity.NOTE_MODE, it)
         }
     }
+    if(clearTask) intent.addFlags(FLAG_ACTIVITY_CLEAR_TASK).addFlags(FLAG_ACTIVITY_NEW_TASK)
     startActivity(intent)
 }
